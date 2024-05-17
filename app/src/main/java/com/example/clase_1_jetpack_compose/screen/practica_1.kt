8be2c48144b2ce87277ffff4a3a13e9cafcc6189
unknown <SENA@MEDAPRCPDFOD047.FormacionSena.RED>
2024-05-17 17:38:32 -0500

    Actualizacion

--- app/src/main/java/com/example/clase_1_jetpack_compose/screen/practica_1.kt
+++ app/src/main/java/com/example/clase_1_jetpack_compose/screen/practica_1.kt
@@ -4,15 +4,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.clase_1_jetpack_compose.R
 
 @Composable
@@ -24,23 +32,36 @@ fun Main(){
 @Composable
  fun Content(){
      Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceAround){
-         Image_1()
-         Texto()
-         Image_1()
+         Filas(R.drawable.img, descripcion ="##")
+
+         Text(text = "heey baby. No fue facil convenceerte", modifier = Modifier
+             .fillMaxWidth()
+             .padding(20.dp),
+             textAlign = TextAlign.Center,
+             fontSize= 20.sp
+         )
+
+         Filas(R.drawable.img_1, descripcion ="##")
+
      }
  }
 
+
 @Composable
-fun Image_1(){
-    Image(painter = painterResource(id = R.drawable.img),
-        contentDescription = null,
-        modifier = Modifier.size(width = 200.dp, height = 200.dp)
-    )
+fun Filas(
+    img:Int,
+    descripcion:String,
+    modifier: Modifier=Modifier.fillMaxWidth(),
+    imgModifier : Modifier = Modifier
+        .width(450.dp)
+        .height(250.dp)
+){
+    Row(modifier = modifier, horizontalArrangement = Arrangement.Center) {
+        Image(painter = painterResource(id = img ), contentDescription = descripcion, modifier = imgModifier )
+    }
+
 }
-@Composable
-fun Texto(){
-    Text(text= "hola")
-}
+
 
 
 @Preview(showSystemUi = true, device = Devices.DEFAULT)
